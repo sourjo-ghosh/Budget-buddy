c71d62b1665c1e67aea67e6cff04ef542802a3a0
sourjo-ghosh <joyghosh211@outlook.com>
2026-04-03 17:29:38 +0600

    dashboard hardcoded ui

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,11 +1,126 @@
-import React from 'react';
+import { BookOpen, Coffee } from "lucide-react";
+import React from "react";
 
 const Dashboard = () => {
-    return (
-        <div>
-            
+  const today = new Date();
+  const date = today.toLocaleDateString("en-BD", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  });
+  const time = today.toLocaleTimeString("en-BD", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  const Name = localStorage.getItem("Name");
+  const Budget = localStorage.getItem("Budget");
+  const spent = 200;
+  const percentage = Math.min((spent / Budget) * 100, 100);
+  console.log(Name);
+  console.log(date, time);
+  return (
+    <div className="p-5">
+      <div className="">
+        <p className="text-4xl text-white">Hey, {Name}</p>
+        <p className="mt-4 text-white/80">{date}</p>
+      </div>
+      <div className="mt-10 p-5 bg-[#1C2739] rounded-2xl border border-gray-200/40">
+        <p className="text-white/70">Spent This Month</p>
+        <p className="text-3xl text-white">
+          ৳ 200/<span className="text-xl text-white/80">{Budget}</span>
+        </p>
+        <div
+          className="bg-violet-500 h-2 rounded-full mt-4 overflow-y-auto"
+          style={{ width: `${percentage}%` }}
+        ></div>
+      </div>
+      <div className="mt-8 grid grid-cols-2 gap-3">
+        <div className="bg-[#1C2739] py-3 px-3 rounded-2xl border border-gray-200/40">
+          <div className="text-pink-400 flex items-center gap-2">
+            <BookOpen></BookOpen>
+            <span>Education</span>
+          </div>
+          <p className="text-white/80 text-2xl mt-3">৳ 200</p>
         </div>
-    );
+        <div className="bg-[#1C2739] py-3 px-3 rounded-2xl border border-gray-200/40">
+          <div className="text-pink-400 flex items-center gap-2">
+            <BookOpen></BookOpen>
+            <span>Education</span>
+          </div>
+          <p className="text-white/80 text-2xl mt-3">৳ 200</p>
+        </div>
+        <div className="bg-[#1C2739] py-3 px-3 rounded-2xl border border-gray-200/40">
+          <div className="text-pink-400 flex items-center gap-2">
+            <BookOpen></BookOpen>
+            <span>Education</span>
+          </div>
+          <p className="text-white/80 text-2xl mt-3">৳ 200</p>
+        </div>
+        <div className="bg-[#1C2739] py-3 px-3 rounded-2xl border border-gray-200/40">
+          <div className="text-pink-400 flex items-center gap-2">
+            <BookOpen></BookOpen>
+            <span>Education</span>
+          </div>
+          <p className="text-white/80 text-2xl mt-3">৳ 200</p>
+        </div>
+        <div className="bg-[#1C2739] py-3 px-3 rounded-2xl border border-gray-200/40">
+          <div className="text-pink-400 flex items-center gap-2">
+            <BookOpen></BookOpen>
+            <span>Education</span>
+          </div>
+          <p className="text-white/80 text-2xl mt-3">৳ 200</p>
+        </div>
+        <div className="bg-[#1C2739] py-3 px-3 rounded-2xl border border-gray-200/40">
+          <div className="text-pink-400 flex items-center gap-2">
+            <BookOpen></BookOpen>
+            <span>Education</span>
+          </div>
+          <p className="text-white/80 text-2xl mt-3">৳ 200</p>
+        </div>
+      </div>
+      <div className="mt-10">
+        <p className="text-white/90 text-xl">Recent Transactions</p>
+        <div>
+          <div className="bg-[#1c2739] py-6 px-5 rounded-2xl border border-gray-200/40 mt-4 flex justify-between items-center">
+            <p className="text-white/80 flex items-center gap-3">
+            <Coffee className="text-pink-400"></Coffee> Coffee 
+            </p>
+            <span className="text-white">Today</span>
+            <span className="text-white/80">-৳ 200</span>
+          </div>
+          <div className="bg-[#1c2739] py-6 px-5 rounded-2xl border border-gray-200/40 mt-4 flex justify-between items-center">
+            <p className="text-white/80 flex items-center gap-3">
+            <Coffee className="text-pink-400"></Coffee> Coffee 
+            </p>
+            <span className="text-white">Today</span>
+            <span className="text-white/80">-৳ 200</span>
+          </div>
+          <div className="bg-[#1c2739] py-6 px-5 rounded-2xl border border-gray-200/40 mt-4 flex justify-between items-center">
+            <p className="text-white/80 flex items-center gap-3">
+            <Coffee className="text-pink-400"></Coffee> Coffee 
+            </p>
+            <span className="text-white">Today</span>
+            <span className="text-white/80">-৳ 200</span>
+          </div>
+          <div className="bg-[#1c2739] py-6 px-5 rounded-2xl border border-gray-200/40 mt-4 flex justify-between items-center">
+            <p className="text-white/80 flex items-center gap-3">
+            <Coffee className="text-pink-400"></Coffee> Coffee 
+            </p>
+            <span className="text-white">Today</span>
+            <span className="text-white/80">-৳ 200</span>
+          </div>
+          <div className="bg-[#1c2739] py-6 px-5 rounded-2xl border border-gray-200/40 mt-4 flex justify-between items-center">
+            <p className="text-white/80 flex items-center gap-3">
+            <Coffee className="text-pink-400"></Coffee> Coffee 
+            </p>
+            <span className="text-white">Today</span>
+            <span className="text-white/80">-৳ 200</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,33 +4,33 @@ import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="sticky bottom-2">
-      <nav>
-        <ul className="p-7 rounded-full flex justify-center items-center gap-8 bg-[#1E293B]">
+    <div className="sticky bottom-2 bg-transparent z-50 w-full flex justify-center items-center">
+      <nav className="bg-transparent">
+        <ul className="px-3 py-2 rounded-full flex justify-center items-center gap-10 bg-white/20 backdrop-blur-md border border-white/20">
           <NavLink
             to="/"
-            className={({ isActive }) => `flex flex-col items-center ${isActive ? "text-[#7f56c5]" : "text-white"}`}
+            className={({ isActive }) => `px-4 py-5 ${isActive ? "text-red-500 rounded-full bg-gray-600" : "text-white"}`}
           >
-            <House></House> Dashboard
+            <House size={30}></House>
           </NavLink>
           <NavLink
             to="/add"
-            className={({ isActive }) => `flex flex-col items-center ${isActive ? "text-[#7f56c5]" : "text-white"}`}
+            className={({ isActive }) => `px-4 py-5 ${isActive ? "text-red-500 rounded-full bg-gray-600" : "text-white"}`}
           >
-            <Plus></Plus> Add
+            <Plus size={30}></Plus> 
           </NavLink>
           <NavLink
             to="/transactions"
-            className={({ isActive }) => `flex flex-col items-center ${isActive ? "text-[#7f56c5]" : "text-white"}`}
+            className={({ isActive }) => `px-4 py-5 ${isActive ? "text-red-500 rounded-full bg-gray-600" : "text-white"}`}
 
           >
-            <Receipt></Receipt> Transactions
+            <Receipt size={30}></Receipt>
           </NavLink>
           <NavLink
             to="/settings"
-            className={({ isActive }) => `flex flex-col items-center ${isActive ? "text-[#7f56c5]" : "text-white"}`}
+            className={({ isActive }) => `px-4 py-5 ${isActive ? "text-red-500 rounded-full bg-gray-600" : "text-white"}`}
           >
-            <Settings></Settings> Settings
+            <Settings size={30}></Settings>
           </NavLink>
         </ul>
       </nav>
